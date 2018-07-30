e28054d4bd401a514541817f9849485b54088fdb
Erik Pukinskis <erik@snowedin.net>
2018-07-29 19:00:24 -0700

    Season 1 Episode 1, Bee bros: script

---
+++ bee-bros.js
@@ -0,0 +1,94 @@
+
+
+var ep = [
+  element("h1", "Warrens"),
+  element("h2", "Episode 1"),
+  element("h3", "Bee bros")]
+
+htmlPainting("foot")
+htmlPainting("sandal")
+
+htmlPainting.stroke(
+  "foot",
+  "brown",{
+  minX: 0,
+  maxX: 12,
+  minY: 0.5,
+  maxY: 1.5})
+
+htmlPainting.stroke(
+  "sandal",
+  "blue",{
+  minX: -0.1,
+  maxX: 12.1,
+  minY: -0.1,
+  maxY: 0.6})
+
+htmlPainting.stroke(
+  "sandal",
+  "blue",{
+  minX: 6,
+  maxX: 10,
+  minY: 0,
+  maxY: 1.5})
+
+htmlPainting.stroke(
+  "foot",
+  "brown",{
+  minX: 6,
+  maxX: 10,
+  minY: 0,
+  maxY: 1.5})
+
+
+site.start(8338)
+
+
+// A foot in a  sandal. Just a brown rectangle and blue footbed and a blue toe hold, a leg and a knee, all rects, maybe a toe
+
+// You stomp left, you stomp right in the grass.
+
+// A bee, just some black and yellow lines, crawls on you yells "ATTACK!" and stings you
+ 
+// The camera changes to top down
+
+// You see <<PAIN>> on the screen, and you can drag the shoes off the feet. If you do the bee falls out
+
+// <<PAIN>> stops for a brief moment
+
+// You can  look for the stinger, i.e. drag to scroll around. It's very close by. As soon as you see it (or after a second, whichever comes first) <<PAIN>> continues
+
+// When you find the stinger, you can choose which angle to pull it out and decides how much of the stinger is left
+
+// After you pull it out you can squeeze your leg, and if you do it quickly you can squeeze out a little poison
+
+// After that you can just look around, you can look at the stinger on your finger
+
+// You can also look at the bee, at which point the camera slow zooms in and a voiceover starts, nature documentary style "The worker bee, in using his stinger, has fatally wounded himself. He will die soon.
+
+// Now you are the bee, and you are in Player V Player with another player. If they don't kill you, you are able to crawl for a bit. After enough crawling you can wiggle your wings. After a bit of that, you can fly short hops.
+
+// You can fly around the workshop, but the world is lit to direct you down a path to a hole in the ground, the hive.
+
+// Before you get there, some of your friend bees land nearby to you.
+
+// You reach out a USB drive.
+
+// You can continue forward to upload the other player's DNA into one of your friends. You start talking either way, bros, there was a homie out  there stomping around crunching stuff up, I had to sting him, I'm dying bros" 
+
+// If you plugged in, you say "take this DNA. learn from it. I love you bros"
+
+// Bro bees hug. You can snuggle up to them if you want, then you die.
+
+// Screen says "dedicated to all the men who died for their queen."
+
+// If you were the human in PvP mode, if you pick up a shoe and stomp the bee, or otherwise squish it, you just see the bee flash backward through its life, to its bros and finally the queen.
+
+// If you let the bee live, you get to watch it play out the game in spectator mode.
+
+// If you choose not to play at all, not to stomp, then you can watch the bees as long as you like. You see the homie bee, the bros, other bees, buzzing around and finding flowers in the grass.
+
+// The grass is blades with two rects, a base and a highlight, that can wave in the wind. You just need like 10 or whatever of them, to show the wave, and then behind that will be a lower fi representation, probably just darker diamonds bobbing as the wave, with square highlights rolling over them.
+
+// Announce the game. It's $0.99. Announce that you're giving away free codes in the reddit thread. After a few hours, give 100 codes to someone and say they can give away codes while you are away, and you will be back at 10am.
+
